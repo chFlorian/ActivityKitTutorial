21a720e42951a05d1542a612c02b4796d048fb7a
Florian Schweizer <post.an.flo@gmail.com>
2022-08-10 15:01:40 +0200

    Initial Commit

---
+++ ActivityKitTutorial/ActivityKitTutorialApp.swift
@@ -0,0 +1,12 @@
+// Created by Florian Schweizer on 10.08.22
+
+import SwiftUI
+
+@main
+struct ActivityKitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
